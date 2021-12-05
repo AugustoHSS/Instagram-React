7ef2d67bd39ce206e95ee4aff53e842248b4265b
Augusto <augustohansel@hotmail.com>
2021-12-04 21:39:21 -0300

    componentizacao com props

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from './Header'
+import Corpo from './Corpo'
+import FundoMobile from './FundoMobile'
+
+export default function App(){
+    return(
+    <div>
+        <Header />
+        <Corpo />
+        <FundoMobile />
+    </div>
+    );
+
+
+}
